bbd465f19c28eb8df5964f1b76783d5013e057dd
Diego Alves <diegohsalves@gmail.com>
2023-01-24 20:08:19 -0300

    Inicio do projeto

---
+++ backend/src/main/java/com/barbershop/BarbershopApplication.java
@@ -0,0 +1,13 @@
+package com.barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/barbershop/BarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
